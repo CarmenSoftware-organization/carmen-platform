d31496d2a9f9704bfbb929acd50f8dfafe8710ac
Thammanoon Semapru <samutpra@users.noreply.github.com>
2026-07-01 11:20:12 +0700

    feat: update Table component to use Fluent UI

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -1,10 +1,18 @@
 import * as React from "react"
 import { cn } from "../../lib/utils"
+import {
+  Table as FluentTable,
+  TableBody as FluentTableBody,
+  TableCell as FluentTableCell,
+  TableHeader as FluentTableHeader,
+  TableHeaderCell as FluentTableHeaderCell,
+  TableRow as FluentTableRow,
+} from "@fluentui/react-table"
 
 const Table = React.forwardRef<HTMLTableElement, React.HTMLAttributes<HTMLTableElement>>(
   ({ className, ...props }, ref) => (
     <div className="relative w-full rounded-lg">
-      <table
+      <FluentTable
         ref={ref}
         className={cn("w-full caption-bottom text-sm", className)}
         {...props}
@@ -16,14 +24,14 @@ Table.displayName = "Table"
 
 const TableHeader = React.forwardRef<HTMLTableSectionElement, React.HTMLAttributes<HTMLTableSectionElement>>(
   ({ className, ...props }, ref) => (
-    <thead ref={ref} className={cn("[&_tr]:border-b-2 [&_tr]:border-border bg-muted", className)} {...props} />
+    <FluentTableHeader ref={ref} className={cn("[&_tr]:border-b-2 [&_tr]:border-border bg-muted", className)} {...props} />
   )
 )
 TableHeader.displayName = "TableHeader"
 
 const TableBody = React.forwardRef<HTMLTableSectionElement, React.HTMLAttributes<HTMLTableSectionElement>>(
   ({ className, ...props }, ref) => (
-    <tbody
+    <FluentTableBody
       ref={ref}
       className={cn("[&_tr:last-child]:border-0", className)}
       {...props}
@@ -48,7 +56,7 @@ TableFooter.displayName = "TableFooter"
 
 const TableRow = React.forwardRef<HTMLTableRowElement, React.HTMLAttributes<HTMLTableRowElement>>(
   ({ className, ...props }, ref) => (
-    <tr
+    <FluentTableRow
       ref={ref}
       className={cn(
         "border-b transition-colors zebra-row data-[state=selected]:bg-muted",
@@ -62,7 +70,7 @@ TableRow.displayName = "TableRow"
 
 const TableHead = React.forwardRef<HTMLTableCellElement, React.ThHTMLAttributes<HTMLTableCellElement>>(
   ({ className, ...props }, ref) => (
-    <th
+    <FluentTableHeaderCell
       ref={ref}
       className={cn(
         "h-11 px-3 text-left align-middle font-semibold text-muted-foreground text-xs uppercase tracking-wider [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
@@ -76,7 +84,7 @@ TableHead.displayName = "TableHead"
 
 const TableCell = React.forwardRef<HTMLTableCellElement, React.TdHTMLAttributes<HTMLTableCellElement>>(
   ({ className, ...props }, ref) => (
-    <td
+    <FluentTableCell
       ref={ref}
       className={cn(
         "p-3 align-middle [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
