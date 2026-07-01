88b943053c828a81dd6796d3bab6576128c0ab8c
Thammanoon Semapru <samutpra@users.noreply.github.com>
2026-07-01 10:43:55 +0700

    feat: migrate Label component to Fluent UI

---
+++ src/components/ui/fluent-label.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Label as FluentLabel, type LabelProps as FluentLabelProps } from '@fluentui/react-components';
+
+interface LabelProps extends FluentLabelProps {}
+
+const Label = React.forwardRef<HTMLLabelElement, LabelProps>(
+  ({ className, children, ...props }, ref) => (
+    <FluentLabel ref={ref} className={className} {...props}>
+      {children}
+    </FluentLabel>
+  )
+);
+Label.displayName = 'Label';
+
+export { Label };
+export type { LabelProps };
--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,19 +1,2 @@
-import * as React from "react"
-import { cn } from "../../lib/utils"
-
-const Label = React.forwardRef<HTMLLabelElement, React.LabelHTMLAttributes<HTMLLabelElement>>(
-  ({ className, ...props }, ref) => (
-    // eslint-disable-next-line jsx-a11y/label-has-associated-control
-    <label
-      ref={ref}
-      className={cn(
-        "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
-        className
-      )}
-      {...props}
-    />
-  )
-)
-Label.displayName = "Label"
-
-export { Label }
+export { Label } from './fluent-label';
+export type { LabelProps } from './fluent-label';
